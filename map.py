b85afbc2569246840ba13fbfc62a897d5108f335
Kirk Kenney <kirk_kenney_1@hotmail.co.uk>
2019-04-24 20:24:30 +0100

    added comments, cleaned up unused code

--- map.py
+++ map.py
@@ -1,8 +1,8 @@
 import folium
-from folium.features import DivIcon
 import pandas as pd
 
 
+# get data from Excel sheet as lists
 data = pd.read_excel('largestcompanies.xlsx')
 company = list(data['Company Name'])
 values = list(data['Market value (USD billions)'])
@@ -10,7 +10,9 @@ location = list(data['Headquarters'])
 latitude = list(data['Latitude'])
 longitude = list(data['Longitude'])
 rank = list(data['Rank'])
+# set world gdp value for later calculations
 world_gdp = 88000.00
+# empty list to latter append values to calculate running total
 totals = []
 total_value = 0
 
@@ -30,25 +32,34 @@ padding: 15px 10px">
 '''
 
 
+# create map object
 map = folium.Map(location=[0, 0], zoom_start=2)
+# create folium feature group
 fg_markers = folium.FeatureGroup(name='World\'s Largest Companies')
+# loop through columns in Excel sheet
 for company, value, loc, lat, lon, rank in zip(company, values, location, latitude, longitude, rank):
+    # add company market values to totals list
     totals.append(value)
+    # calculate company value as a decimal of world gdp
     percent = value/world_gdp
+    # format company value as a %
     percent = '{:.2%}'.format(percent)
+    # create folium iframe object
     iframe = folium.IFrame(html=html % (company, loc, rank, value, percent), width=300, height=150)
+    # define parameters for company markers: popup boxes, tooltips etc
     fg_markers.add_child(folium.CircleMarker(location=[lat, lon],
         popup=folium.Popup(iframe), fill=True,
         color='black', fill_color='blue',
         fill_opacity=0.6, radius=12, tooltip='Rank #' + str(rank)))
 
 for total in totals:
+    # loop through company values in totals list, adding to running total
     total_value += total
 
+# get total values as a decimal proportion of world gdp
 proportion = total_value/world_gdp
+# format values as a % of world gdp
 proportion = '{:.2%}'.format(proportion)
-print(total_value)
-print(proportion)
 
 map.add_child(fg_markers)
 map.get_root().html.add_child(folium.Element(legend_html % (total_value, proportion)))
--- scrape.py
+++ scrape.py
@@ -17,39 +17,19 @@ rank = 0
 
 for company in companies:
     rank += 1
+    # Google HQ for each company in data set
     hq_search = BeautifulSoup(requests.get(search + urllib.parse.quote_plus(company) + '+headquarters', headers=userAgent).text, 'html.parser')
+    # get values from 1st result displayed in Google search
     hq = hq_search.find('div', {'class': 'Z0LcW'}).get_text()
+    # get latitutde and longitude of city where company HQ is located
     location = geolocator.geocode(hq)
     lat = location.latitude
     lon = location.longitude
-    # data.loc[company, 'Headquarters'] = hq
+    # append latitude to Excel sheet
     data.loc[company, 'Latitude'] = lat
+    # append longitude to Excel sheet
     data.loc[company, 'Longitude'] = lon
+    # append rank integer to Excel sheet
     data.loc[company, 'Rank'] = rank
+    # save amended Excel sheet
     data.to_excel(data_loc, index=False)
-    print(company + ' added')
-    # coord_search = BeautifulSoup(requests.get(search + urllib.parse.quote_plus(hq) + '+coordinates', headers=userAgent).text, 'html.parser')
-    # coord = coord_search.find('div', {'class': 'Z0LcW'}).get_text()
-    # lat, lon = coord.split(', ')
-    # print(company + ': ' + 'LAT ' + lat + ', LON ' + lon)
-
-# import pandas as pd
-# import requests as requests
-# import urllib
-# from bs4 import BeautifulSoup
-# datasetLocation = "largestcompanies.xlsx"
-# df = pd.read_excel(datasetLocation, "Sheet1")
-# userAgent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
-# searchURL = "https://www.google.com/search?q="
-# for x in range(0, len(df.index)):
-#     companyName = df.iloc[x, 0]
-#     search1 = BeautifulSoup(requests.get(searchURL + urllib.parse.quote_plus(companyName) + '+headquarters ', headers=userAgent).text, 'html.parser')
-#     headquarters = search1.find('div', {"class": "Z0LcW"}).get_text()
-#     if not headquarters:
-#         headquarters = search1.find('div', {"class": "desktop-title-subcontent"}).get_text()
-#     search2 = BeautifulSoup(requests.get(searchURL + urllib.parse.quote_plus(headquarters) + '+coordinates', headers=userAgent).text, 'html.parser')
-#     coordinates = search2.find('div', {"class": "Z0LcW"}).get_text()
-#     df.loc[x, 'Location'] = headquarters
-#     df.loc[x, 'Coordinates'] = coordinates
-#     print(df.iloc[x, 0] + " | " + headquarters + " | " + coordinates)
-# df.to_excel(datasetLocation, index=False)
